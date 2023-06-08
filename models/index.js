ec985cbec262cad293ea40c104029a491c3c6d7e
Andre Besner <besner.andre@gmail.com>
2023-06-08 14:59:26 -0400

    add models for user, comments, and blog posts, add structure for relations

---
+++ models/index.js
@@ -0,0 +1,15 @@
+const User = require('./User');
+const Comment = require('./Comment');
+const Blog = require('./Blog');
+
+//user has many blog posts
+
+//user has many comments
+
+// comments belong to one user
+
+//comments belong to one blog post
+
+//blog posts belongs to one user
+
+//blog has has many comments
