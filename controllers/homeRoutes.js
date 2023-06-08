449d31241366a3dc9ba6f4713cffab44716cd53a
Andre Besner <besner.andre@gmail.com>
2023-06-08 17:29:52 -0400

    add more routing data to controller folder

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -65,3 +65,42 @@ router.get("/blog/:id", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+router.get('/profile', withAuth, async (req, res) => {
+    try {
+      // Find the logged in user based on the session ID
+      const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ['password'] },
+        include: [{ model: Blog }],
+      });
+  
+      const user = userData.get({ plain: true });
+  
+      res.render('profile', {
+        ...user,
+        logged_in: true
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/profile');
+      return;
+    }
+  
+    res.render('login');
+  });
+
+  router.get('/signUp', (req, res) => {
+	if (req.session.logged_in) {
+		res.redirect('/profile');
+		return;
+	}
+	res.render('signUp');
+});
+
+module.exports = router;
